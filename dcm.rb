cd3ab542cd450a888c79f252f014d22e05983b72
Dmitry Zhifarsky <dimannich2380@gmail.com>
2023-08-14 17:08:35 +0400

    build: update version

--- dcm.rb
+++ dcm.rb
@@ -3,14 +3,14 @@ class Dcm < Formula
   homepage "https://dcm.dev/"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.1/dcm-macos-x64-release.zip"
-    sha256 "b25ec64575b3d34632267ed635d3f3e5184623a70594ae3c56963cc2aa3bf7af"
+    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.2/dcm-macos-x64-release.zip"
+    sha256 "64a097a2dcca5e3771ea0d9275c787d4564746d4b53a26c621e6b2f937a79dcb"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.1/dcm-macos-arm-release.zip"
-    sha256 "c8825ce56498d7a93531bed62b25137b0baa5de9f9eb30f683252c1478b2e7bc"
+    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.2/dcm-macos-arm-release.zip"
+    sha256 "55a15defa84a1d59423863fbf4b12ebcb27762c16eedc67180d81dbb4910862b"
   elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.1/dcm-linux-x64-release.zip"
-    sha256 "fbceeaa7dbbd9c12d8079c21ecf0301c60ac7af1f89a372eab04bb56eb704191"
+    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.2/dcm-linux-x64-release.zip"
+    sha256 "229d3722fa805d8580d8b72c0481a57278df939edf76f93078b9db50f41f4d9a"
   end
 
   def install
---
+++ dcm@1.8.1.rb
@@ -0,0 +1,23 @@
+class DcmAT181 < Formula
+  desc "DCM statically analyzes your Dart code to quickly find quality and consistency problems."
+  homepage "https://dcm.dev/"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.1/dcm-macos-x64-release.zip"
+    sha256 "b25ec64575b3d34632267ed635d3f3e5184623a70594ae3c56963cc2aa3bf7af"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.1/dcm-macos-arm-release.zip"
+    sha256 "c8825ce56498d7a93531bed62b25137b0baa5de9f9eb30f683252c1478b2e7bc"
+  elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    url "https://github.com/CQLabs/homebrew-dcm/releases/download/1.8.1/dcm-linux-x64-release.zip"
+    sha256 "fbceeaa7dbbd9c12d8079c21ecf0301c60ac7af1f89a372eab04bb56eb704191"
+  end
+
+  def install
+    bin.install "dcm"
+  end
+
+  test do
+    system "false"
+  end
+end
